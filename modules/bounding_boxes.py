25c35edf3dbce11cbd3d04bea18831cfed6b61f8
chrisdianru <32670282+chrisdianru@users.noreply.github.com>
2021-01-27 11:45:21 +0800

    Update for OIDv6

--- modules/bounding_boxes.py
+++ modules/bounding_boxes.py
@@ -15,7 +15,7 @@ def bounding_boxes_images(args, DEFAULT_OID_DIR):
 		dataset_dir = os.path.join(DEFAULT_OID_DIR, args.Dataset)
 		csv_dir = os.path.join(DEFAULT_OID_DIR, 'csv_folder')
 
-	name_file_class = 'class-descriptions-boxable.csv'
+	name_file_class = 'class-descriptions-boxable.csv' #https://storage.googleapis.com/openimages/v5/class-descriptions-boxable.csv
 	CLASSES_CSV = os.path.join(csv_dir, name_file_class)
 
 	if args.command == 'downloader':
@@ -32,7 +32,9 @@ def bounding_boxes_images(args, DEFAULT_OID_DIR):
 			args.multiclasses = 0
 
 		folder = ['train', 'validation', 'test']
-		file_list = ['train-annotations-bbox.csv', 'validation-annotations-bbox.csv', 'test-annotations-bbox.csv']
+		file_list = ['oidv6-train-annotations-bbox.csv', # https://storage.googleapis.com/openimages/v6/oidv6-train-annotations-bbox.csv
+			     'validation-annotations-bbox.csv', #https://storage.googleapis.com/openimages/v5/validation-annotations-bbox.csv
+			     'test-annotations-bbox.csv'] # https://storage.googleapis.com/openimages/v5/test-annotations-bbox.csv
 
 		if args.classes[0].endswith('.txt'):
 			with open(args.classes[0]) as f:
